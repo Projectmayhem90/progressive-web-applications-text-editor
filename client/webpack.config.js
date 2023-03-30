3d7fd8867424a7c58c964778898f9a0b779a8d41
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-03-29 21:27:30 -0400

    Updated Webpack to fix heroku issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,69 +1,67 @@
-//Const webpack 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// Models to sync to get on heroku
+const config = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'JATE',
+    }),
 
-module.exports = () => {
-  const config = {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE',
-      }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
 
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
-
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E.',
-        description: 'Take notes with JS',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512], //added for screen sizes
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-    ],
-
-    module: {
-      rules: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'J.A.T.E.',
+      description: 'Take notes with JS',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      icons: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
         },
       ],
-    },
-  };
-
-  return config;
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
 };
+
+module.exports = config;
